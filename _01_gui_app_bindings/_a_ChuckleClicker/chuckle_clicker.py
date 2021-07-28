8e7a46f37594b4ea4d6ffdd51c204b4c0f27a4d0
DemirKaya7 <74899222+DemirKaya7@users.noreply.github.com>
2021-07-27 18:32:33 -0700

    july27
    july 27 2021

--- _01_gui_app_bindings/_a_ChuckleClicker/chuckle_clicker.py
+++ _01_gui_app_bindings/_a_ChuckleClicker/chuckle_clicker.py
@@ -17,29 +17,36 @@ class ChuckleClicker(tk.Tk):
 
         # TODO: Declare and initialize 2 buttons (tk.Button)
         #  one button for the joke and another for the punchline
-
+        jokeButton = tk.Button(self, text="Joke")
+        punchlineButton = tk.Button(self, text="Punchline")
         # TODO: Place the 2 buttons next to each other (see example image)
-
+        jokeButton.place(relx=0, rely=0, relwidth=0.5, relheight=1)
+        punchlineButton.place(relx=0.5, rely=0, relwidth=0.5, relheight=1)
         # TODO: Call the joke button's bind() method to call the on_joke()
         #  method when a mouse button is pressed
         #  example: self.joke_button.bind('<ButtonPress>', self.on_joke)
+        jokeButton.bind('<ButtonPress>', self.on_joke)
 
         # TODO: Call the joke button's bind() method to call the on_punchline()
         #  method when a mouse button is pressed
+        punchlineButton.bind('<ButtonPress>', self.on_punchline)
+
 
     def on_joke(self, event):
-        print('Joke button pressed')
-
         # TODO: Write your joke below!
+        print("What did the pirate say when he turned 80?")
 
     def on_punchline(self, event):
-        print('Punchline button pressed')
-
         # TODO: Write a punchline to your joke!
+        print("Aye Matey")
 
 
 if __name__ == '__main__':
-    pass
+
     # TODO: Create a new ChuckleClicker object and set the title and geometry.
     #  Remember to call mainloop() at the end
+    x = ChuckleClicker()
+    x.title("Chuckle clicker")
+    x.geometry("500x500")
+    x.mainloop()
 
--- _01_gui_app_bindings/_b_TypingTutor/typing_tutor.py
+++ _01_gui_app_bindings/_b_TypingTutor/typing_tutor.py
@@ -15,16 +15,17 @@ class TypingTutor(tk.Tk):
 
         # TODO: Declare and initialize a member variable to hold a
         #  random letter to type
-
+        self.letter = generate_random_letter()
         # TODO: Declare and initialize a label (tk.Label) and set the text to the random letter
-
+        self.label = tk.Label(self, text=self.letter)
         # TODO: Place the label in the center of the window
-
+        self.label.place(relx=0.45, rely=0.45, relwidth=0.1, relheight=0.1)
         # TODO: Call the label's focus_set() method so key presses can be detected
-
+        self.label.focus_set()
         # TODO: Call the label's bind() method to call the on_key_release()
         #  method when a key is released
         #  example: self.label.bind('<KeyRelease>', self.on_key_release)
+        self.label.bind('<KeyRelease>', self.on_key_release)
 
 
     def on_key_release(self, event):
@@ -36,6 +37,10 @@ class TypingTutor(tk.Tk):
         #       If the user pressed the wrong key,
         #         change the label's background to red
         #  example: self.label.configure(bg='green')
+        if letter_pressed == self.letter:
+            self.label.configure(bg="Green")
+        else:
+            self.label.configure(bg="Red")
 
         # TODO: Get a new random letter and place it on the label
         #  example: self.label.configure(text=self.rand_letter)
@@ -45,3 +50,8 @@ if __name__ == '__main__':
     pass
     # TODO: Create a new _b_TypingTutor object and set the title and geometry.
     #  Remember to call mainloop() at the end
+    x = TypingTutor()
+    x.title = "Typing Tutor"
+    x.geometry("500x500")
+    x.mainloop()
+
